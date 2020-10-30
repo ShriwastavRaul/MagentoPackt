c17d7cac737cd4354ba1d26c2b8fa1250187e99d
Rahul Shriwastav <21.shriwastavrhaul@gmail.com>
2020-10-30 16:02:58 -0700

    first commit hai bhaya

---
+++ src/testmyWebDriver/samplePro.java
@@ -0,0 +1,21 @@
+package testmyWebDriver;
+
+import DummyInt.FireFoxDriver;
+import DummyInt.IEdriver;
+
+public class samplePro {
+
+	public static void main(String[] args)
+	{
+	
+		//FireFoxDriver driver=new FireFoxDriver();
+		
+		IEdriver driver=new IEdriver();
+		
+		driver.click("Submit");
+		driver.sendKey("text box", "whatever");
+		
+
+	}
+
+}
